39de3e34f749f3bddd763968e80331eab93c2163
habaisuniversegod <102759337+habaisuniversegod@users.noreply.github.com>
2022-05-22 23:21:47 +0300

    навёл марафет

---
+++ mental/preload-fonts.js
@@ -0,0 +1,5 @@
+let htmlData = `<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Mulish:wght@300&family=Podkova&display=swap" rel="stylesheet">`;
+
+document.write(htmlData);
--- mental/timer.js
+++ mental/timer.js
@@ -1,12 +1,21 @@
-let counter = document.getElementById("counter"); 
+let counter = document.getElementById("counter");
+let header = document.getElementById('header-script') 
 let time = 20;
+let headerAnimCounter = 1;
 let timer = setInterval(() => count(), 1000);
+let headerAnimTimer = setInterval(() => headAnim(), 650);
 let result = document.getElementById("result");
 
 function count(){
 	time--;
 	counter.innerHTML = time;
 	if (time == 0){
-		result.outerHTML = "<a href='result.html' id='res-a'>Посмотреть результат</a>";
+		result.outerHTML = "<a href='result.html' id='res-a' title='Тыкай!'><b>Посмотреть результат</b></a>";
 	}
+}
+
+function headAnim(){
+	headerAnimCounter++;
+	header.innerHTML = "Что-то грузим"+".".repeat(headerAnimCounter);
+	if (headerAnimCounter == 3) headerAnimCounter = 0;
 }
