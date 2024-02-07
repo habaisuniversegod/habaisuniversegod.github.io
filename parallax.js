fa25de2fc8eb8b20cfcc4a709a8c2fe4e844c542
habaisuniversegod <habalyn228@outlook.com>
2024-02-07 19:55:00 +0300

    Main nakonetsto gotoff

--- parallax.js
+++
@@ -1,4 +0,0 @@
-let bodyElement = document.getElementsByTagName('body')[0];
-                let htmlElement = document.getElementsByTagName('html')[0];
-                let scroll = 0;
-                window.addEventListener('scroll', () => {scroll = htmlElement.scrollTop; bodyElement.style.backgroundPosition = '0 '+scroll/1.5+'px'})
