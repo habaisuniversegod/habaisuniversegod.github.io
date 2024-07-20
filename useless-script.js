0c0314c319895af0e98f99f72ddb38523e52ec7c
habaisuniversegod <asmfemboy6@debian.haba>
2024-07-20 17:21:03 +0300

    20.07.2024 upd (index upd)

---
+++ useless-script.js
@@ -0,0 +1,6 @@
+let huynya = `link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&family=Ubuntu:wght@300&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="style.css">
+		<link rel="shortcut icon" href="icon.ico" type="image/x-icon">`
+document.write(huynya);
