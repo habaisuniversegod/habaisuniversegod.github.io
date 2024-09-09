5eb27d30a70cebd12d546fd74c282299aa296318
habaisuniversegod <asmfemboy6@debian.haba>
2024-09-09 21:30:48 +0400

    upd. 9 sep 2024 21:30 utc+4

--- mental/scroll.js
+++
@@ -1,9 +0,0 @@
-let scrollOffsetY = document.getElementsByTagName('html')[0].scrollTop;
-let bd = document.getElementsByTagName('body')[0];
-
-function bgOffset(){
-	scrollOffsetY = document.getElementsByTagName('html')[0].scrollTop;
-	bd.style.backgroundPosition = "0 "+scrollOffsetY/1.5+"px";
-}
-
-window.addEventListener('scroll', bgOffset);
